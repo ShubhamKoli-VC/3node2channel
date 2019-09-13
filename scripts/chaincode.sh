c843e4dcad93d50f93a32854e60d00dda900bc69
ShubhamKoli-VC <shubham.koli@viitor.cloud>
2019-09-13 10:29:41 +0530

    added comments

--- scripts/chaincode.sh
+++ scripts/chaincode.sh
@@ -1,8 +1,10 @@
+# this script will installe the chaincode and instantiate it.
+
 peer channel create -o orderer.example.com:7050 -c channelall -f ../channel-artifacts/channelall.tx
 
 peer channel join -b channelall.block
 
-peer chaincode install -n stress -v 1.0 -l node -p /opt/gopath/src/github.com/hyperledger/fabric/peer/stress-test
+peer chaincode install -n stress -v 1.0 -l node -p /opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/stress-test
 
 peer chaincode instantiate -o orderer.example.com:7050 -C channelall -n stress -l node -v 1.0 -c '{"function":"initLedger","Args":[]}' -P "OR ('Org1MSP.member','Org2MSP.member','Org3MSP.member','Org4MSP.member')"
 
--- scripts/joinchannel.sh
+++ scripts/joinchannel.sh
@@ -1,4 +1,7 @@
-peer chaincode install -n stress -v 1.0 -l node -p /opt/gopath/src/github.com/hyperledger/fabric/peer/stress-test
+#To be tun by the peers who have not installed chaincode or joined channel
+
+
+peer chaincode install -n stress -v 1.0 -l node -p /opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/stress-test
 
 peer channel fetch 0 channelall.block -o orderer.example.com:7050 -c channelall
 
