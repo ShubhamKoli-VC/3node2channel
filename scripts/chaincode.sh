e1f6c77f9373e199fcad9208774034216bb242a1
ShubhamKoli-VC <shubham.koli@viitor.cloud>
2019-09-19 10:19:24 +0530

    added new script file for shrim and updated to  support orderer

--- scripts/chaincode.sh
+++ scripts/chaincode.sh
@@ -2,7 +2,7 @@
 
 peer channel create -o orderer.example.com:7050 -c channelall -f ../channel-artifacts/channelall.tx
 
-peer channel join -b channelall.block
+peer channel join -b channelall.block -o orderer.example.com:7050
 
 peer chaincode install -n stress -v 1.0 -l node -p /opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/stress-test
 
---
+++ scripts/joinchannel-shrim.sh
@@ -0,0 +1,8 @@
+#To be tun by the peers who have not installed chaincode or joined channel
+
+
+peer chaincode install -n stress -v 1.0 -l node -p /opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/shrim
+
+peer channel fetch 0 channel12.block -o orderer.example.com:7050 -c channel12
+
+peer channel join -b channel12.block -o orderer.example.com:7050
