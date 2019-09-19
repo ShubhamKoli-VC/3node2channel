ad996b3f85fe7b0902989ca477cc7add7dd46331
ShubhamKoli-VC <shubham.koli@viitor.cloud>
2019-09-19 10:49:03 +0530

    small fix

--- scripts/channel12.sh
+++ scripts/channel12.sh
@@ -4,4 +4,4 @@ peer channel join -b channel12.block -o orderer.example.com:7050
 
 peer chaincode install -n shrim -v 1.0 -l node -p /opt/gopath/src/github.com/hyperledger/fabric/peer/channel-artifacts/shrim
 
-peer chaincode instantiate -o orderer.example.com:7050 -C channelall -n shrim -l node -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
+peer chaincode instantiate -o orderer.example.com:7050 -C channel12 -n shrim -l node -v 1.0 -c '{"Args":[""]}' -P "OR ('Org1MSP.member','Org2MSP.member')"
